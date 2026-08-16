fd61487faf4f023b1905f0e00837143cca804b65
vasicekmilan90-eng <vasicek.milan.90@gmail.com>
2026-08-16 09:43:15 +0200

    Refactor process_message method for attribute updates
    Refactor process_message to update attributes from message dynamically.

--- midealan/devices/c3/__init__.py
+++ midealan/devices/c3/__init__.py
@@ -323,7 +323,11 @@ class MideaC3Device(MideaDevice):
         """Midea C3 device process message."""
         message = MessageC3Response(msg)
         _LOGGER.debug("[%s] Received: %s", self.device_id, message)
-        new_status = self.update_attributes_from_message(message)
+        new_status: dict[str, Any] = {}
+        for status in self._attributes:
+            if hasattr(message, str(status)):
+                self._attributes[status] = getattr(message, str(status))
+                new_status[str(status)] = getattr(message, str(status))
         if "zone_temp_type" in new_status:
             for zone in [0, 1]:
                 if self._attributes[DeviceAttributes.zone_temp_type][
